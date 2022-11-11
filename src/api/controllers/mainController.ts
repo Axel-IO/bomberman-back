09d419a1ea60de4ee2d700c55222adc9a6961037
Axel-IO <axel.joigneau@innovorder.fr>
2022-11-11 18:48:01 +0100

    first commit

---
+++ src/api/controllers/mainController.ts
@@ -0,0 +1,83 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnConnect,
+  OnDisconnect,
+  OnMessage,
+  SocketController,
+  SocketIO,
+} from "socket-controllers";
+import { Socket, Server } from "socket.io";
+
+@SocketController()
+export class MainController {
+  socketList = [];
+  players = {};
+
+  constructor() {}
+
+  @OnConnect()
+  public onConnection(
+    @ConnectedSocket() socket: Socket,
+    @SocketIO() io: Server
+  ) {
+    console.log("New Socket connected: ", socket.id);
+
+    this.socketList.push(socket);
+    this.players[socket.id] = {
+      x: 50,
+      y: 50,
+      size: 20,
+      speed: 5,
+      color: "#" + (((1 << 24) * Math.random()) | 0).toString(16),
+    };
+
+    setInterval(() => {
+      socket.emit("update_players", this.players);
+    }, 1000 / 60);
+
+    socket.on("custom_event", (data: any) => {
+      console.log("Data: ", data);
+    });
+  }
+
+  @OnDisconnect()
+  public onDisconnection(
+    @ConnectedSocket() socket: Socket,
+    @SocketIO() io: Server
+  ) {
+    console.log("Socket disconnected: ", socket.id);
+
+    delete this.players[socket.id];
+  }
+
+  @OnMessage("move_player")
+  public movePlayer(
+    @SocketIO() io: Server,
+    @ConnectedSocket() socket: Socket,
+    @MessageBody()
+    body: { maxWidth: number; maxHeight: number; direction: string }
+  ) {
+    this.moveOnePlayer(body, this.players[socket.id]);
+    socket.emit("update_players", this.players);
+  }
+
+  public moveOnePlayer({ direction, maxWidth, maxHeight }, player) {
+    if (direction == "UP") player.y -= player.speed;
+    if (direction == "DOWN") player.y += player.speed;
+    if (direction == "LEFT") player.x -= player.speed;
+    if (direction == "RIGHT") player.x += player.speed;
+
+    const borderSize = player.size/2
+    // const borderSize = 0
+    if (player.y - borderSize <= 0) player.y = borderSize;
+    if (player.y + borderSize >= maxHeight) player.y = maxHeight - borderSize;
+    if (player.x - borderSize <= 0) player.x = borderSize;
+    if (player.x + borderSize >= maxWidth) player.x = maxWidth - borderSize;
+
+    // if (player.y <= 0) player.y = 0;
+    // if (player.y + borderSize >= maxHeight) player.y = maxHeight - borderSize;
+    // if (player.x <= 0) player.x = 0;
+    // if (player.x + borderSize >= maxWidth) player.x = maxWidth - borderSize;
+  }
+}
